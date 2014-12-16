81598db6081cd86bb838553b41471b010f0b1de2
mrasu <m.rasu.hitsuji@gmail.com>
2014-12-16 22:54:14 +0900

    fix error

--- viewer/views.py
+++ viewer/views.py
@@ -16,5 +16,5 @@ def index(request):
 def discover(request, discover_id):
     discover_target = get_object_or_404(NewDiscover, pk=discover_id)
 
-    context = {'link_set': discover_target.newdiscoverlink_set.all()}
+    context = {'link_set': discover_target.links.all()}
     return render(request, 'viewer/discover.html', context)
